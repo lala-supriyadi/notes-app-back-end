972dea5b4c4f12ee1d0416aa5d2175b2554f4846
lala-supriyadi <supriyadi.lala@gmail.com>
2023-03-21 16:06:27 +0700

    export notes implemented

--- src/server.js
+++ src/server.js
@@ -18,7 +18,10 @@ const AuthenticationsValidator = require('./validator/authentications');
 const collaborations = require('./api/collaborations');
 const CollaborationsService = require('./services/postgres/CollaborationsService');
 const CollaborationsValidator = require('./validator/collaborations');
-
+// Exports
+const _exports = require('./api/exports');
+const ProducerService = require('./services/rabbitmq/ProducerService');
+const ExportsValidator = require('./validator/exports');
 
  
 const init = async () => {
@@ -91,6 +94,13 @@ const init = async () => {
         collaborationsService,
         notesService,
         validator: CollaborationsValidator,
+      },
+    },
+    {
+      plugin: _exports,
+      options: {
+        service: ProducerService,
+        validator: ExportsValidator,
       },
     },
 ]);
